38dde09dad0e7f16a4e6dbd708fab625fe1d3c22
OfekBaram <ofekbaram5@gmail.com>
2023-04-24 00:01:18 +0300

    fixes

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  const destination = document.querySelector('#destination').value;
-  const startDate = document.querySelector('#start-date').value;
-  const returnDate = document.querySelector('#return-date').value;
-  const numberOfTravelers = document.querySelector('#travelers-number').value;
-
-
   window.location.href = 'loading-page.html';
 });
 
